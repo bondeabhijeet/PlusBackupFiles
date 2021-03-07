927fb07065273e9922690c189cb0f5139a52f36e
bondeabhijeet <bondeabhijeetunderveil@gmail.com>
2021-03-07 20:06:20 +0530

    Multiple File support added

--- first.py
+++ first.py
@@ -2,57 +2,85 @@ import requests
 import tkinter
 from tkinter import filedialog
 import sys
+import time
 
+msg = str()
+WriteMessage = str()
+FileNumber = int()
 
 ErrorCodes = {"10":"ERROR: File is not provided", "11":"ERROR: File is empty", "12":"ERROR: File is invalid", "20":"ERROR: Wait for an hour before the next upload", "21":"ERROR: wait for 24 hours before you're next upload", "22":"ERROR: Too many bytes sent in a hour, wait for 1 hour", "23":"ERROR: Too many bytes sent in a day, wait for 24 hours", "30":"ERROR: File type not supported by the server", "31":"ERROR: File size is too big", "32":"ERROR: File banned", "40":"ERROR: System error"}
 
+# -------------------- FUNCTIONS --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def BackupFile(path, FileNumber):
+    global msg
+    
+    print("\nSelected file is:", path)
+    
+    print(f'Trying to UPLOAD file# 0{FileNumber}...\n')
+    RecievedResponse = requests.post('https://api.anonfiles.com/upload', files = {'file' : open(path, 'rb' ) } ) # It is strongly recommended that you open files in binary mode. This is because Requests may attempt to provide the Content-Length header for you, and if it does this value will be set to the number of bytes in the file. Errors may occur if you open the file in text mode.
+                                                                                      # https://www.tutorialspoint.com/requests/requests_file_upload.htm                                                                       
+    
+        
+    if (RecievedResponse.ok):
+        NameOfFile = RecievedResponse.json()["data"]["file"]["metadata"]["name"]
+        SizeOfFile = RecievedResponse.json()["data"]["file"]["metadata"]["size"]["readable"]
+        LongUrl = RecievedResponse.json()["data"]["file"]["url"]["full"]
+        ShortUrl = RecievedResponse.json()["data"]["file"]["url"]["short"]
+        FileID = RecievedResponse.json()["data"]["file"]["metadata"]["id"]
+        print(">> [ File is uploaded ]")
+        print(">> [ NAME:     ", NameOfFile, "]")
+        print(">> [ SIZE:     ", SizeOfFile, "]")
+        print(">> [ LONG URL: ", LongUrl, "]")
+        print(">> [ SHORT URL:", ShortUrl, "]")
+        print(">> [ FILE ID:  ", FileID, " (Save this file id if you want to retrive this file in future without the URL.) ]\n\n")
+        msg = f"{NameOfFile}  ||  {ShortUrl}  || {FileID}  ||  {SizeOfFile}  ||  {LongUrl}"
+        return(msg)
+    else:
+        print(ErrorCodes[ str(RecievedResponse.json()["error"]["code"])])
+        print("\n\n>>>> [ Upload was UNSUCCESSFUL. ]\n>>>> [ Press any key to Continue... ]\n\n")
+        input()
+        sys.exit()
+
+def FileName():
+    Days = {'0':'Monday', '1':'Tuesday', '2':'Wednesday', '3':'Thrusday', '4':'Friday', '5':'Saturday', '6':'Sunday'}
+    Month = {'1':'Jan', '2':'Feb', '3':'Mar', '4':'Apr', '5':'May', '6':'Jun', '7':'Jul', '8':'Aug', '9':'Sept', '10':'Oct', '11':'Nov', '12':'Dec'}
+    FileDayAndMonth = f'{Days[ str(time.localtime().tm_wday)] } {Month[ str(time.localtime().tm_mon)] }'
+    FileTime = "%02d-%02d-%02d" % (time.localtime().tm_hour, time.localtime().tm_min, time.localtime().tm_sec)
+    FileDate = "%04d-%02d-%02d" % (time.localtime().tm_year, time.localtime().tm_mon, time.localtime().tm_mday)
+    return( f'{FileDate} {FileTime} {FileDayAndMonth}.txt' )
+
+def WriteToFile(WriteMessage):
+    root = tkinter.Tk()
+
+    TextFilePath = filedialog.asksaveasfilename(parent = root, defaultextension = ".txt", filetypes = [("Text files", ".txt")], title = "Saving Text File", initialfile = FileName()) # the (initialdir = "C:\\") attribute is not filled coz if its absent then by default it takes the path to downloads folder. (although there is no official documentaion on this.)
+    with open(TextFilePath, 'w') as TextWriting:
+        TextWriting.write(WriteMessage)
+    print(">> [ OUTPUT FILE CREATED ]\n")
+    root.destroy()
+
+# -------------------- SELECT FILES TO UPLOAD -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 root = tkinter.Tk()  # The root window is created. The root window is a main application window in our programs. It has a title bar and borders. These are provided by the window manager. It must be created before any other widgets.
-# root is the master window
-
-
+                     # root is the master window
 root.title('BackupPlusFiles') # Name given the the popup window
-
-path = filedialog.askopenfilename(parent = root, title = "File To Backup", initialdir = "C:\\", filetypes = [("All Files", ".*")] ) # The askopenfilename() function returns the file name that you selected. here "All Files" is a Label (So it can be given any name)
+paths = filedialog.askopenfilename(parent = root, title = "File To Backup", filetypes = [("All Files", ".*")], multiple = True ) # The askopenfilename() function returns the file name that you selected. here "All Files" is a Label (So it can be given any name)
+                                                                                                                                 # the (initialdir = "C:\\") attribute is not filled coz if its absent then by default it takes the path to downloads folder. (although there is no official documentaion on this.)
 root.destroy()
 
 
-print("Selected file is:", path)
-try:
-    RecievedResponse = requests.post('https://api.anonfiles.com/upload', files = {'file' : open(path, 'rb' ) } ) # It is strongly recommended that you open files in binary mode. This is because Requests may attempt to provide the Content-Length header for you, and if it does this value will be set to the number of bytes in the file. Errors may occur if you open the file in text mode.
-                                                                                      # https://www.tutorialspoint.com/requests/requests_file_upload.htm
+# -------------------- MAIN -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-except:
-    print("ERROR... SELECT A FILE TO BACKUP")
-    print("Press enter to continue")
-    input()
-    sys.exit()
-if (RecievedResponse.ok):
-  NameOfFile = RecievedResponse.json()["data"]["file"]["metadata"]["name"]
-  SizeOfFile = RecievedResponse.json()["data"]["file"]["metadata"]["size"]["readable"]
-  LongUrl = RecievedResponse.json()["data"]["file"]["url"]["full"]
-  ShortUrl = RecievedResponse.json()["data"]["file"]["url"]["short"]
-  FileID = RecievedResponse.json()["data"]["file"]["metadata"]["id"]
-  print(">> File is uploaded")
-  print(">> NAME:     ", NameOfFile)
-  print(">> SIZE:     ", SizeOfFile)
-  print(">> LONG URL: ", LongUrl)
-  print(">> SHORT URL:", ShortUrl)
-  print(">>>> FILE ID:", FileID, " (Save this file id if you want to retrive this file in future without the URL.)")
+for path in paths:
+  FileNumber += 1
+  RecicvedMessage = BackupFile(path, FileNumber)
+  WriteMessage = WriteMessage +"\n" + RecicvedMessage
+
+if (paths):
+    WriteToFile(WriteMessage)
 else:
-  print(ErrorCodes[ str(RecievedResponse.json()["error"]["code"])])
+    print("\n\n No Output File Created... (no file was selected)\n\n")
 
-NameOfFile = RecievedResponse.json()["data"]["file"]["metadata"]["name"]
-
-root = tkinter.Tk()
-
-TextFilePath = filedialog.asksaveasfilename(parent = root, initialdir = "C:\\", defaultextension = ".txt", filetypes = [("Text File", '.txt')], title = "Saving the link in text file", initialfile = f'{NameOfFile}.txt')
-# https://docs.python.org/3/library/dialog.html#tkinter.filedialog.SaveAs
-
-with open( TextFilePath, 'w') as TextWriting:
-  TextWriting.write(f"{NameOfFile}  ||  {ShortUrl}  ||  {SizeOfFile}  ||  {LongUrl}")
-
-root.destroy()
-
+# -------------------- HOLD THE SCREEN --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 print("PRESS ENTER TO CONTINUE")
 input()
