44611b94f182944c289149a1ba6e71779adb7a4c
bondeabhijeet <bondeabhijeetunderveil@gmail.com>
2021-04-14 17:11:02 +0530

    ProgressBar Added

--- PlusRestoreFiles.py
+++ PlusRestoreFiles.py
@@ -4,9 +4,14 @@ import tkinter
 from tkinter import filedialog
 import sys
 
-FileID = input("Enter the FileID :")
+#------------------------------------------------------------------------------INPUT & PROCESSING------------------------------------------------------------------------------------------------------
 
-FileLink = requests.get(f'https://anonfiles.com/{FileID}').text
+FileID = input("Enter the FileID :")
+Fileinfo = requests.get(f'https://api.anonfiles.com/v2/file/{FileID}/info')     # Fetching File info using API
+FilePageLink = Fileinfo.json()['data']['file']['url']['short']                  # Extracting File page url from the info recieved
+FileSizeInBytes = Fileinfo.json()['data']['file']['metadata']['size']['bytes']  # Extracting File Size from the info recieved
+
+FileLink = requests.get(FilePageLink).text                                      # Getting the raw html code of the FileDownload page for parsing through BeautifulSoup
 
 soup = BeautifulSoup(FileLink, 'lxml')
 
@@ -18,25 +23,43 @@ except:
     print(f"\n [ ERROR: {ErrorText} ]")
     sys.exit()
 
+#------------------------------------------------------------------------------FUNCTIONS---------------------------------------------------------------------------------------------------------------
 
 def GetPath():
     root = tkinter.Tk()
     root.withdraw()
     root.call('wm', 'attributes', '.', '-topmost', '1')  # To bring the "Saving the file" window to top
-    path = filedialog.asksaveasfilename(parent = root, filetypes = [('All Files', '.*')], initialfile = FileName, title = 'Saving the file')
+    Path = filedialog.asksaveasfilename(parent = root, filetypes = [('All Files', '.*')], initialfile = FileName, title = 'Saving the file')
     root.destroy()
-    return(path)
-    
-path = GetPath()
+    return(Path)
 
-if(path):
-    print(" [ Downloading File ] ", end = '\r')
+def Downloader(URL, TotalSize, Path):              # TotalSize in bytes in string type
+    r = requests.get(URL, stream = True)
+    print("\nFile Name:", FileName, f"[{round((TotalSize/1048576), 2)} MB]")
 
-    FileData = requests.get(DirectDownloadLink)    
-    with open(path, 'wb') as p:
-        p.write(FileData.content)
-    print(" [ File Downlaoded Successfully ]")
-        
+    TotalBytesRecieved = 0
+    with open (f'{Path}', 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            TotalBytesRecieved += len(chunk)     # The Total number of bytes recieved 
+            PercentageUsed = ((TotalBytesRecieved)/(int(TotalSize)))*100  # To find the Percentage of the downloaded part (ie. (Total bytes recieved/Total size) * 100 )
+
+            status = '[' + str(int(PercentageUsed)*'>').ljust(100, '-') + '] ' + f"{round(TotalBytesRecieved/1048576, 2)} MB  " + f"[{round(PercentageUsed, 2)}%] "
+            # To indicate the status of the download we use status string
+            # ljust : https://www.geeksforgeeks.org/python-string-ljust-rjust-center/
+            # round : https://kodify.net/python/math/round-decimals/#round-decimal-places-up-and-down-round
+
+            print(status, end='\r')
+
+            f.write(chunk)
+    print(f"{status}[ Download Complete ]")
+
+#------------------------------------------------------------------------------MAIN--------------------------------------------------------------------------------------------------------------------
+
+Path = GetPath()
+
+if(Path):
+    Downloader(DirectDownloadLink, FileSizeInBytes, Path)        
 else:
-    print(" [ ERROR: No path selected ] ")
-    
+    print(" [ ERROR: No Path selected ] ")
+
+#------------------------------------------------------------------------------END---------------------------------------------------------------------------------------------------------------------
